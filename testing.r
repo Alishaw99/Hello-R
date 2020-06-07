e752020c9cef654695dc6dee94a147798d87c29d
Alishaw99 <tariqham@gmail.com>
2020-06-07 10:48:05 +0500

    Few lines are added

--- testing.r
+++ testing.r
@@ -1,2 +1,15 @@
 # This is from Git
 Print('Hell R!')
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+name = "Tariq"
+print("name")
